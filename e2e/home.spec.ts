02aef5e53709c1194ef6152344d8db747067e20f
Gabriella Braz <>
2025-07-14 10:47:40 -0300

    voltando para const title e removendo o timeout

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -6,15 +6,4 @@ test('Deve exibir o título', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Linkaí/);
 
-  await page.waitForTimeout(3000)
 });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
