ba0e75a01457eea017737a61bb99b14a97a283dc
Gabriella Braz <bibiellabraz@gmail.com>
2025-07-17 21:25:37 -0300

    tirando o teste negativo

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -23,29 +23,3 @@ test('deve logar com sucesso', async ({ page }) => {
 
     await expect(page.locator('h1')).toContainText(`Olá, ${user.name}! 👋`);
 });
-
-
-test('Não deve logar com senha incorreta', async ({ page }) => {
-
-       const user = {
-        name: 'gabriella',
-        username: 'gabriella',
-        password: '123456'
-    }
-
-    await page.goto('http://localhost:3000/login');
-
-    await page
-        .getByRole('textbox', { name: 'Seu @username incrível' })
-        .fill(user.username);
-
-    await page
-        .getByRole('textbox', { name: 'Digite sua senha secreta' })
-        .fill(user.password);
-
-    await page
-        .getByRole('button', { name: 'Entrar' })
-        .click();
-
-  await expect(page.getByRole('status')).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
-});
