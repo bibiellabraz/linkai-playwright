2012860a35c20fb08bc3f530c8e0897e23858d91
Gabriella Braz <bibiellabraz@gmail.com>
2025-07-17 21:59:01 -0300

    Subindo três testes automatizados

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test'
 
+// import { writeFileSync } from 'fs'
+
 test('deve logar com sucesso', async ({ page }) => {
     const user = {
         name: 'gabriella',
@@ -22,4 +24,58 @@ test('deve logar com sucesso', async ({ page }) => {
         .click();
 
     await expect(page.locator('h1')).toContainText(`Olá, ${user.name}! 👋`);
+});
+
+test('Não deve logar com senha incorreta', async ({ page }) => {
+    const user = {
+        name: 'gabriella',
+        username: 'gabriella',
+        password: '123456'
+    }
+
+    await page.goto('http://localhost:3000/login');
+
+    await page
+        .getByRole('textbox', { name: 'Seu @username incrível' })
+        .fill(user.username);
+
+    await page
+        .getByRole('textbox', { name: 'Digite sua senha secreta' })
+        .fill(user.password);
+
+    await page
+        .getByRole('button', { name: 'Entrar' })
+        .click();
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
+});
+
+test('Não deve logar com usuário não cadastrado', async ({ page }) => {
+    const user = {
+        name: 'gabriella',
+        username: 'not-found',
+        password: '123456'
+    }
+
+    await page.goto('http://localhost:3000/login');
+
+    await page
+        .getByRole('textbox', { name: 'Seu @username incrível' })
+        .fill(user.username);
+
+    await page
+        .getByRole('textbox', { name: 'Digite sua senha secreta' })
+        .fill(user.password);
+
+    await page
+        .getByRole('button', { name: 'Entrar' })
+        .click();
+
+    const toast = page.locator('.toast')
+
+    await expect(toast).toContainText('Oops!')
+    await expect(toast).toContainText('Algo deu errado com seu login. Por favor, verifique suas credenciais e tente novamente.');
 });
